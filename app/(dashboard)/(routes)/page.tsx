02c6f3d1640563b91bb1aa3f0c661991648aa626
Yank <yankleebr@gmail.com>
2024-01-05 21:21:57 -0300

    Authentication

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,5 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <p>This is protected Page!</p>
+    <div>
+      <UserButton
+        afterSignOutUrl="/"
+      />
+    </div>
   )
 }
