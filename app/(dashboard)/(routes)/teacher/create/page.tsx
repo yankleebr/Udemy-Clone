685efb82e9077797f03b7c9c414775848e3018d9
Yank <yankleebr@gmail.com>
2024-01-10 09:43:21 -0300

    Course Creation UI

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -3,8 +3,14 @@
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
-import { useRouter } from 'next/router'
+
 import axios from 'axios'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import { useRouter } from 'next/navigation'
+import toast from 'react-hot-toast'
 
 const formSchema = z.object({
   title:z.string().min(1,{
@@ -13,6 +19,8 @@ const formSchema = z.object({
 })
 
 const CreatePage = () =>{
+  const router = useRouter()
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver:zodResolver(formSchema),
     defaultValues:{
@@ -22,15 +30,68 @@ const CreatePage = () =>{
 
   const {isSubmitting, isValid} = form.formState
 
-  const onSubmit = (values:z.infer<typeof formSchema>) =>{
-    console.log(values)
+  const onSubmit = async (values:z.infer<typeof formSchema>) =>{
+    try {
+      const response = await axios.post('/api/course', values)
+      router.push(`/teacher/courses/${response.data.id}`)
+    } catch  {
+      toast.error('Something went wrong')
+    }
   }
   return(
-    <div className='max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6'>
+    <div className='max-w-5xl mx-auto md:items-center md:justify-center h-full p-6'>
       <div>
-        <h1>
+        <h1 className='text-2xl'>
           Name your course
         </h1>
+        <p className='text-sm text-slate-600'>
+          What would you like to name your course? Don't worry, you can change this later.
+        </p>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className='space-y-8 mt-8'
+          >
+            <FormField
+              control={form.control}
+              name='title'
+              render={({field}) =>(
+                <FormItem>
+                  <FormLabel>
+                    Course Title  
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g 'Advanced web developmemt'" 
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    What will you teach in this course?
+                  </FormDescription>
+                  <FormMessage/>
+                </FormItem>  
+              )}
+            />
+            <div className='flex items-center gap-x-2'>
+                <Link href={'/'}>
+                  <Button
+                    type='button'
+                    variant={'ghost'}
+                  >
+                    Cancel
+                  </Button>
+                </Link>
+                <Button
+                  type='submit'
+                  disabled={!isValid || isSubmitting}
+                >
+                  Continue
+                </Button>
+            </div>
+          </form>
+        </Form>
       </div>
     </div>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+import { ToastProvider } from '@/components/providers/toaster-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,7 +22,12 @@ export default function RootLayout({
         <head>
           <link rel='icon' href='/favicon.ico'/>
         </head>
-        <body className={inter.className}>{children}</body>
+
+        <body className={inter.className}>
+          <ToastProvider/>
+          {children}
+        </body>
+
       </html>
     </ClerkProvider>
   )
