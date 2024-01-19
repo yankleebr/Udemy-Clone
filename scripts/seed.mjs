37cf552c55582bd5b5f4127448efb86a96f01118
Yank <yankleebr@gmail.com>
2024-01-19 20:39:53 -0300

    Course Category Form

---
+++ scripts/seed.mjs
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+
+
+const database = new PrismaClient()
+
+async function main () {
+  try {
+    await database.category.createMany({
+      data:[
+        {name:"Computer Science"},
+        {name:"Music"},
+        {name:"Fitness"},
+        {name:"Photography"},
+        {name:"Accounting"},
+        {name:"Engineering"}
+      ]
+    })
+    console.log('Sucess')
+  } catch (error) {
+    console.log('Error seeding the database categories', error)
+  } finally{
+    await database.$disconnect()
+  }
+}
+
+main()
