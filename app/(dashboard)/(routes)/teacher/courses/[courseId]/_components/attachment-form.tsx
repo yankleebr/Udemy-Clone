8da52d5c25bae0ea772598c65725bda2551d4b96
Yank <yankleebr@gmail.com>
2024-01-25 21:44:10 -0300

    course attachment form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
@@ -2,7 +2,7 @@
 
 import * as z from 'zod'
 import { Button } from '@/components/ui/button'
-import { ImageIcon, Pencil, PlusCircle } from 'lucide-react'
+import { File, ImageIcon, Loader2, Pencil, PlusCircle, X } from 'lucide-react'
 import { useState } from 'react'
 import toast from 'react-hot-toast'
 import axios from 'axios'
@@ -26,6 +26,7 @@ export const AttachmentForm = ({
 }:AttachmentFormProps) =>{
 
   const [isEditing, setIsEditing] = useState(false)
+  const [deletingId, setDeletingId] = useState<string | null>(null)
 
   const toggleEdit = () => setIsEditing((current) => !current)
 
@@ -33,7 +34,7 @@ export const AttachmentForm = ({
 
   const onSubmit = async (values:z.infer<typeof formSchema>) =>{
     try {
-      await axios.patch(`/api/courses/${courseId}`, values)
+      await axios.post(`/api/courses/${courseId}/attachments`, values)
       toast.success('Course Update')
       toggleEdit()
       router.refresh()
@@ -41,6 +42,20 @@ export const AttachmentForm = ({
       toast.error('Something went wrong')
     }
   }
+
+  const onDelete = async (id:string) =>{
+    try {
+      setDeletingId(id)
+      await axios.delete(`/api/courses/${courseId}/attachments/${id}`)
+      toast.success('Attachment deleted')
+      router.refresh()
+    } catch  {
+      toast.error('Something went wrong')
+    }finally{
+      setDeletingId(null)
+    }
+  }
+
   return(
     <div className='mt-6 border bg-slate-100 rounded-md p-4'>
       <div className='font-medium flex items-center justify-between'>
@@ -61,25 +76,52 @@ export const AttachmentForm = ({
       </div>
       {!isEditing && (
         <>
-          {initialData.attachments === 0 && (
+          {initialData.attachments.length === 0 && (
             <p className='text-sm mt-2 text-slate-500 italic'>
               No attachments yet
             </p>
+          )}
+          {initialData.attachments.length > 0 &&(
+            <div className='space-y-2'>
+              {initialData.attachments.map((attachment) =>(
+                <div
+                  key={attachment.id}
+                  className='flex items-center p-3 w-full bg-sky-100 border-sky-200 border text-sky-700 rounded-md'
+                >
+                  <File className='h-4 w-4 mr-2 flex-shrink-0'/>
+                  <p className='text-xs line-clamp-1'>
+                    {attachment.name}
+                  </p>
+                  {deletingId === attachment.id &&(
+                    <div>
+                      <Loader2 className='h-4 w-4 animate-spin'/>
+                    </div>
+                  )}
+                  {deletingId !== attachment.id &&(
+                    <button
+                     onClick={() => onDelete(attachment.id)}
+                     className='ml-auto hover:opacity-75 transition'>
+                      <X className='h-4 w-4'/>
+                    </button>
+                  )}
+                </div>  
+              ))}
+            </div>
           )}
         </>
       )}
       {isEditing &&(
        <div>
         <FileUpload
-          endpoint='courseImage'
+          endpoint='courseAttachment'
           onChange={(url) =>{
             if(url){
-              onSubmit({imageUrl:url})
+              onSubmit({url:url})
             }
           }}
         />
         <div className='text-xs text-muted-foreground mt-4'>
-            16:9 aspect ratio recommended
+            Add anything your students might need to complete the course.
         </div>
        </div>
       )}
