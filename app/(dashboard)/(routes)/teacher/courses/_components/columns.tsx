2b16fa3c689acb67f741cad01db3c08d8dc54c16
Yank <yankleebr@gmail.com>
2024-02-15 13:53:31 -0300

    Search Query

--- app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -97,6 +97,7 @@ export const columns: ColumnDef<Course>[] = [
             <Link href={`/teacher/courses/${id}`}>
               <DropdownMenuItem>
                 <Pencil className="h-4 w-4 mr-2"/>
+                Edit
               </DropdownMenuItem>
             </Link>
           </DropdownMenuContent>
