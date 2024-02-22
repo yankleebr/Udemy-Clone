9d717e959a424080f0e4d633d42a8e26b76033f0
Yank <yankleebr@gmail.com>
2024-02-22 10:00:32 -0300

    course-sidebar-item

--- app/(course)/courses/[courseId]/_component/course-sidebar.tsx
+++ app/(course)/courses/[courseId]/_component/course-sidebar.tsx
@@ -2,6 +2,7 @@ import { db } from "@/lib/db"
 import { auth } from "@clerk/nextjs"
 import { Chapter, Course, UserProgress } from "@prisma/client"
 import { redirect } from "next/navigation"
+import { CourseSidebarItem } from "./course-sidebar-item"
 
 interface CourseSidebarProps{
   course:Course & {
@@ -38,6 +39,18 @@ export const CourseSidebar = async ({
           {course.title}
         </h1>
         {/* check purchase and add progress */}
+      </div>
+      <div className="flex flex-col w-full">
+        {course.chapters.map((chapter) =>(
+          <CourseSidebarItem
+            key={chapter.id}
+            id={chapter.id}
+            label={chapter.title}
+            isCompleted={!!chapter.userProgress?.[0]?.isCompleted}
+            courseId={course.id}
+            isLocked={!chapter.isFree && !purchase}
+          />
+        ))}
       </div>
     </div>
   )
