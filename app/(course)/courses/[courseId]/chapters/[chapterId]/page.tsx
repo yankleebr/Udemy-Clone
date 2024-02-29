c1f4c43888c21d16e45ae2b1a2a321c140bca3e6
Yank <yankleebr@gmail.com>
2024-02-29 11:35:01 -0300

    chapter HLS Video Player - Initial

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,34 +1,9 @@
-import { db } from "@/lib/db"
-import { redirect } from "next/navigation";
-
-
-const  ChapterIdPage = async({
-  params
-}:{
-  params:{ courseId:string }
-}) => {
-  const course = await db.course.findUnique({
-    where:{
-      id:params.courseId,
-    },
-    include:{
-      chapters:{
-        where:{
-          isPublished:true,
-        },
-        orderBy:{
-          position:'asc'
-        }
-      } 
-    }
-  })
-
-  if(!course){
-    return redirect('/')
-  }
-
-  
-  return redirect(`/courses/${course.id}/chapters/${course.chapters[0].id}`)
+const CourseIdPage = () => {
+  return (
+    <div>
+      Course page
+    </div>
+  );
 }
  
-export default ChapterIdPage;
+export default CourseIdPage;
