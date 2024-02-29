bd7418e30bbe04f9a7c583dd0e7fc0aa396254d9
Yank <yankleebr@gmail.com>
2024-02-29 20:55:56 -0300

    Chapter HLS Video Player - part 2

---
+++ actions/get-chapters.ts
@@ -0,0 +1,57 @@
+import { db } from "@/lib/db"
+
+interface GetChaptersProps{
+  userId:string
+  courseId:string
+  chapterId:string
+}
+
+export const getChapter = async ({
+  userId,
+  courseId,
+  chapterId
+}:GetChaptersProps) =>{
+  try {
+    const purchase = await db.purchase.findUnique({
+      where:{
+        userId_courseId:{
+          userId,
+          courseId,
+        }
+      }
+    })
+
+    const course = await db.course.findUnique({
+      where:{
+        isPublished:true,
+        id:courseId
+      },
+      select:{
+        price:true,
+      }
+    })
+
+    const chapter = await db.chapter.findUnique({
+      where:{
+        id:chapterId,
+        isPublished:true,
+      }
+    })
+
+    if (!chapter || !course) {
+      throw new Error("Chapter or course not found")
+    }
+  } catch (error) {
+    console.log("[GET_CHAPTER]", error)
+    return{
+      chapter:null,
+      course:null,
+      muxData:null,
+      attachments:[],
+      nextChapter:null,
+      userProgress:null,
+      purchase:null, 
+      
+    }
+  }
+}
--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,7 +1,19 @@
-const CourseIdPage = () => {
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const CourseIdPage = ({
+  params
+}:{
+  params:{courseId:string, chapterId:string}
+}) => {
+  const { userId } = auth()
+
+  if(!userId){
+    return redirect('/')
+  }
   return (
     <div>
-      Course page
+      Chapter ID!
     </div>
   );
 }
