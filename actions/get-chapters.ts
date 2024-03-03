3e8e17ac226ebaff6e4b5014c35f96805a7f0219
Yank <yankleebr@gmail.com>
2024-03-03 08:35:32 -0300

    Chapter HLS Video Player - Completed

--- actions/get-chapters.ts
+++ actions/get-chapters.ts
@@ -1,4 +1,5 @@
 import { db } from "@/lib/db"
+import { Attachment, Chapter } from "@prisma/client"
 
 interface GetChaptersProps{
   userId:string
@@ -41,6 +42,59 @@ export const getChapter = async ({
     if (!chapter || !course) {
       throw new Error("Chapter or course not found")
     }
+
+    let muxData = null
+    let attachments: Attachment[] = []
+    let nextChapter:Chapter | null = null
+
+    if(purchase){
+      attachments = await db.attachment.findMany({
+        where:{
+          courseId
+        }
+      })
+    }
+
+    if (chapter.isFree || purchase){
+      muxData = await db.muxData.findUnique({
+        where:{
+          chapterId
+        }
+      })
+
+      nextChapter = await db.chapter.findFirst({
+        where:{
+          courseId,
+          isPublished:true,
+          position:{
+            gt:chapter?.position
+          }
+        },
+        orderBy:{
+          position:"asc"
+        }
+      })
+    }
+
+    const userProgress = await db.userProgress.findUnique({
+      where:{
+        userId_chapterId:{
+          userId,
+          chapterId
+        }
+      }
+    })
+
+    return{
+      chapter,
+      course,
+      muxData,
+      attachments,
+      nextChapter,
+      userProgress,
+      purchase, 
+    }
+
   } catch (error) {
     console.log("[GET_CHAPTER]", error)
     return{
--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,7 +1,10 @@
+import { getChapter } from "@/actions/get-chapters";
+import { Banner } from "@/components/banner";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { VideoPlayer } from "./_components/video-player";
 
-const CourseIdPage = ({
+const CourseIdPage = async ({
   params
 }:{
   params:{courseId:string, chapterId:string}
@@ -11,9 +14,56 @@ const CourseIdPage = ({
   if(!userId){
     return redirect('/')
   }
+
+  const {
+    chapter,
+    course,
+    muxData,
+    attachments,
+    nextChapter,
+    userProgress,
+    purchase, 
+  } = await getChapter({
+    userId,
+    chapterId:params.chapterId,
+    courseId:params.courseId
+  })
+
+  if(!chapter || !course){
+    return redirect('/')
+  }
+
+  const isLocked = !chapter.isFree && !purchase
+  const completeOnEnd = !!purchase && !userProgress?.isCompleted
+
   return (
     <div>
-      Chapter ID!
+      {userProgress?.isCompleted && (
+        <Banner
+          variant={"success"}
+          label={"You already completed this chapter"}
+        />
+      )}
+      {isLocked && (
+        <Banner
+          variant={"warning"}
+          label={"You need to purchase this course to whatch this chapter"}
+        />
+      )}
+      <div className="flex flex-col max-w-4xl mx-auto pb-20">
+        <div  className="p-4">
+          <VideoPlayer
+            chapterId={params.chapterId}
+            title={chapter.title}
+            courseId={params.courseId}
+            nextChapterId={nextChapter?.id}
+            playbackId={muxData?.playbackId!}
+            isLocked={isLocked}
+            completeOnEnd={completeOnEnd}
+          
+          />
+        </div>
+      </div>
     </div>
   );
 }
