660e4ea215aee886028837e43872578c8389222e
Yank <yankleebr@gmail.com>
2024-03-30 09:08:27 -0300

    Chapter Progress Functionality

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -7,6 +7,7 @@ import { CourseEnrollButton } from "./_components/course-enroll-button";
 import { Separator } from "@/components/ui/separator";
 import { Preview } from "@/components/preview";
 import { File } from "lucide-react";
+import { CourseProgressButton } from "./_components/course-progress-Button";
 
 const CourseIdPage = async ({
   params
@@ -73,9 +74,12 @@ const CourseIdPage = async ({
               {chapter.title}
             </h2>
             {purchase ?(
-              <div>
-                {/* // TODO: Add CourseProgressButton */}
-              </div>  
+              <CourseProgressButton
+                chapterId={params.chapterId}
+                courseId={params.courseId}
+                nextChapterId={nextChapter?.id}
+                isCompleted={!!userProgress?.isCompleted}
+              />
               
             ):(
               <CourseEnrollButton
