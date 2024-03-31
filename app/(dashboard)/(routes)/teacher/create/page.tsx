fa6380e49e2c2579d543aabb2c1b1530c603902d
Yank <yankleebr@gmail.com>
2024-03-31 10:24:19 -0300

    don´t - error vercel - do not

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -46,7 +46,7 @@ const CreatePage = () =>{
           Name your course
         </h1>
         <p className='text-sm text-slate-600'>
-          What would you like to name your course? Don't worry, you can change this later.
+          What would you like to name your course? Do not worry, you can change this later.
         </p>
         <Form {...form}>
           <form
